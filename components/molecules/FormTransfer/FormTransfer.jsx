ccba0f640d112af2a623ea93b90276bcddd9c288
Federico Jure <fjure@atixlabs.com>
2019-02-28 15:06:04 -0300

    nintegrate with backend, with mockups

--- components/molecules/FormTransfer/FormTransfer.jsx
+++ components/molecules/FormTransfer/FormTransfer.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { Form, Input, Icon } from "antd";
-import ButtonPrimary from '../../atoms/ButtonPrimary/ButtonPrimary.jsx';
+import ButtonPrimary from "../../atoms/ButtonPrimary/ButtonPrimary.jsx";
 
-import './_style.scss';
+import "./_style.scss";
 
-const FormTransfer = () => (
+const FormTransfer = ({ onTransferChange, onAmountChange, submitTransfer }) => (
   <div className="FormTrasnfer">
-    <Form>
+    <Form onSubmit={submitTransfer}>
       <Form.Item label="Transfer Receipt Number">
         <Input
           placeholder="Ej. 123456789"
-          prefix={<Icon type="file-protect" style={{ color: "rgba(0,0,0,.25)" }} />}
+          prefix={
+            <Icon type="file-protect" style={{ color: "rgba(0,0,0,.25)" }} />
+          }
+          onChange={onTransferChange}
         />
       </Form.Item>
       <Form.Item label="Amount">
         <Input
-        placeholder="Ej. $10.000 USD"
-        prefix={<Icon type="dollar" style={{ color: "rgba(0,0,0,.25)" }} />}
-         />
+          placeholder="Ej. $10.000 USD"
+          prefix={<Icon type="dollar" style={{ color: "rgba(0,0,0,.25)" }} />}
+          onChange={onAmountChange}
+        />
       </Form.Item>
       <Form.Item>
-        <ButtonPrimary text="CONFIRM" />
+        <ButtonPrimary text="CONFIRM" onClick={submitTransfer} />
       </Form.Item>
     </Form>
   </div>
