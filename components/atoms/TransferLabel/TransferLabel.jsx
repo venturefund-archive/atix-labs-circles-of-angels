fcf8887100e9f6ad197a43b21b316d9f851c930b
agus <atroiano+kraken@atixlabs.com>
2019-02-28 13:07:32 -0300

    Create Transfer Label component. Create Transfer Funds Confirmation Page.

---
+++ components/atoms/TransferLabel/TransferLabel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './_style.scss';
+
+const TransferLabel = ( {text}) => (
+  <div className="TransferLabel">
+    <p>{text}</p>
+  </div>
+);
+
+export default TransferLabel;
