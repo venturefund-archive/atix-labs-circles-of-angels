b876987229ceb0bc5d194f0240d4e9a880facfea
Leo Gobbi <gobbileonel@gmail.com>
2019-03-21 15:51:47 -0300

    COA-288 add signStatus map

---
+++ model/signStatus.js
@@ -0,0 +1,14 @@
+const signStatusMap = {
+  '0': {
+    show: 'Sign Pending',
+    icon: '/static/images/icon-clock.svg',
+    name: 'pending'
+  },
+  '1': {
+    show: 'Signed Agreement',
+    icon: '/static/images/icon-check.svg',
+    name: 'success'
+  }
+};
+
+export default signStatusMap;
