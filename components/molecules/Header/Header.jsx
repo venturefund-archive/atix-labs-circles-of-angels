73b2fb4a6eaef98ef3874ba8bb700de80538eaed
Federico Jure <fjure@atixlabs.com>
2019-03-25 11:19:31 -0300

    COA-309 inject user provider on general app

--- components/molecules/Header/Header.jsx
+++ components/molecules/Header/Header.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import UserAvatar from '../../molecules/UserAvatar/UserAvatar.jsx';
-import { UserProvider, UserConsumer } from '../../utils/UserContext';
+import { UserConsumer } from '../../utils/UserContext';
 
 import './_style.scss';
 
 const Header = () => (
-  <UserProvider>
-    <div className="HeaderContainer">
-      <div className="RightSide">
-        <UserConsumer>
-          {({ user }) => (
-            <UserAvatar userName={user.name} userRole="Impact Funder" />
-          )}
-        </UserConsumer>
-      </div>
+  <div className="HeaderContainer">
+    <div className="RightSide">
+      <UserConsumer>
+        {({ user }) => (
+          <UserAvatar userName={user.name} userRole="Impact Funder" />
+        )}
+      </UserConsumer>
     </div>
-  </UserProvider>
+  </div>
 );
 
 export default Header;
