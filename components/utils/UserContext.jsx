20b3f849521fdfc597277e2ab15cafb9d2372a08
Federico Jure <fjure@atixlabs.com>
2019-03-26 14:07:38 +0000

    Merge branch 'develop' into 'COA-317-login-routing-depends-role'
    # Conflicts:
    #   pages/create-project-step1.jsx

--- components/utils/UserContext.jsx
+++ components/utils/UserContext.jsx
@@ -1,29 +1,53 @@
 import React from 'react';
 
 const UserContext = React.createContext('user');
+const userKey = 'user';
 
-class UserProvider extends React.Component {
-  state = {
-    user: {
-      id: 1,
-      name: 'Mariano Maidana',
-      
+export const withUser = ComponentToWrap => {
+  return class UserComponent extends React.Component {
+    static contextType = UserContext;
+
+    render() {
+      const { user, changeUser, removeUser } = this.context;
+      return (
+        <ComponentToWrap {...this.props} user={user} changeUser={changeUser} removeUser={removeUser} />
+      );
     }
   };
+};
 
-  loginUser = ({ id, name }) => {};
+export class UserProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    try {
+      const user = JSON.parse(localStorage.getItem(userKey));
+      this.setState({ user });
+    } catch (error) {}
+  }
+
+  changeUser = user => {
+    this.setState({ user });
+    localStorage.setItem(userKey, JSON.stringify(user));
+  };
+
+  removeUser = () => {
+    this.setState({user: {}});
+    localStorage.setItem(userKey, null);
+  }
 
   render() {
     return (
       <UserContext.Provider
-        value={{ user: this.state.user, loginUser: this.loginUser }}
+        value={{ user: this.state.user, changeUser: this.changeUser, removeUser: this.removeUser }}
       >
         {this.props.children}
       </UserContext.Provider>
     );
   }
 }
-
-const UserConsumer = UserContext.Consumer;
-
-export { UserProvider, UserConsumer };
