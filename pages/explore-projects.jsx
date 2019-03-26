c38f5310f047ce85f5568e4dd632659a5d8f4203
Federico Jure <fjure@atixlabs.com>
2019-03-26 09:48:58 -0300

    COA-309 remove unused import

--- pages/explore-projects.jsx
+++ pages/explore-projects.jsx
@@ -4,7 +4,6 @@ import Header from '../components/molecules/Header/Header';
 import SideBar from '../components/organisms/SideBar/SideBar';
 import CardProject from '../components/molecules/CardProject/CardProject';
 import { getProjects } from '../api/projectApi';
-import { UserProvider } from '../components/utils/UserContext';
 import './_style.scss';
 import './_explore-projects.scss';
 
