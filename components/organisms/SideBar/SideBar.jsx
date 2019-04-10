e817b6a363fbfc7f0a7523293a31ded77436049c
Federico Jure <fjure@atixlabs.com>
2019-04-10 14:32:58 -0300

    COA-436 add button on sidebar for backoffice-admin users to route to funds administration

--- components/organisms/SideBar/SideBar.jsx
+++ components/organisms/SideBar/SideBar.jsx
@@ -3,6 +3,7 @@ import { Layout, Menu, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import './_style.scss';
 import Routing from '../../utils/Routes';
+import { withUser } from '../../utils/UserContext';
 
 const { Sider } = Layout;
 
@@ -10,31 +11,45 @@ const goToExploreProjects = () => {
   Routing.toExploreProjects();
 };
 
-const SideBar = () => (
-  <Sider
-    width="70"
-    breakpoint="md"
-    collapsedWidth="0"
-    onBreakpoint={broken => {
-      console.log(broken);
-    }}
-    onCollapse={(collapsed, type) => {
-      console.log(collapsed, type);
-    }}
-  >
-    <div className="logo">
-      <img src="./static/images/circle-isologo.svg" alt="Circles of Angels" />
-    </div>
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-      <Menu.Item key="1" onClick={goToExploreProjects}>
-      <Icon type="appstore" />
-        {/* <img src="./static/images/menu-home.svg" alt="Home" /> */}
-      </Menu.Item>
-      <Menu.Item key="2">
-      <Icon type="sliders" />
-      </Menu.Item>
-    </Menu>
-  </Sider>
-);
+const goToFundsAdministration = () => {
+  Routing.toFundAdministration();
+};
 
-export default SideBar;
+const SideBar = ({ user, isBackofficeAdmin }) =>
+  user ? (
+    <Sider
+      width="70"
+      breakpoint="md"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        // console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        // console.log(collapsed, type);
+      }}
+    >
+      <div className="logo">
+        <img src="./static/images/circle-isologo.svg" alt="Circles of Angels" />
+      </div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" onClick={goToExploreProjects}>
+          <Icon type="appstore" />
+          {/* <img src="./static/images/menu-home.svg" alt="Home" /> */}
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="sliders" />
+        </Menu.Item>
+        {isBackofficeAdmin() ? (
+          <Menu.Item key="3" onClick={goToFundsAdministration}>
+            <Icon type="fund" />
+          </Menu.Item>
+        ) : (
+          ''
+        )}
+      </Menu>
+    </Sider>
+  ) : (
+    ''
+  );
+
+export default withUser(SideBar);
