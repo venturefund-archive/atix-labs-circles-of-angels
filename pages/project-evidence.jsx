c2eecad497f305662d5de6d1b237801315e794f2
Eugenia Romero <eromero@atixlabs.com>
2019-04-12 11:19:17 -0300

    418-modify actiones in evidence table

---
+++ pages/project-evidence.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import {
+  Tabs,
+  message,
+  Divider,
+  Button,
+  Icon,
+  Breadcrumb,
+  Progress,
+  Tooltip
+} from 'antd';
+import Header from '../components/molecules/Header/Header';
+import SideBar from '../components/organisms/SideBar/SideBar';
+import CustomButton from '../components/atoms/CustomButton/CustomButton';
+import './_style.scss';
+import './_concensus.scss';
+import './_project-evidence.scss';
+import './_steps.scss';
+import TableProjectProgress from '../components/organisms/TableProjectProgress/TableProjectProgress';
+import Label from '../components/atoms/Label/Label';
+import DragUploadFile from '../components/molecules/DragUploadFile/DragUploadFile';
+import TableEvidence from '../components/organisms/TableEvidence/TableEvidence';
+
+const BreadCrumb = () => (
+  <Breadcrumb>
+    <Breadcrumb.Item>
+      <Icon type="arrow-left" />
+      <a href="@">Project Progress</a>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>Evidence</Breadcrumb.Item>
+  </Breadcrumb>
+);
+const ProjectEvidence = () => (
+  <div className="AppContainer">
+    <SideBar />
+    <div className="MainContent">
+      <Header />
+      <div className="Content">
+        <div className="DataSteps">
+          <BreadCrumb />
+          <div className="ProjectInfoHeader">
+            <div className="space-between">
+              <div>
+                <Label labelText="project name" />
+                <h1>Women Professional Development</h1>
+              </div>
+            </div>
+          </div>
+          <div className="b-right">
+            <div>
+              <Label labelText="Task name" />
+              <h3>Junior Account Manager Hire</h3>
+            </div>
+            <div className="flex list">
+              <span className="listItem flex">
+                <Tooltip title="Date">
+                  <Icon
+                    style={{ fontSize: '16px', color: '#a3a5a9' }}
+                    type="calendar"
+                  />
+                </Tooltip>
+                20/09/2019
+              </span>
+              <Divider type="vertical" />
+              <span className="listItem flex">
+                <Tooltip title="Amount">
+                  <Icon
+                    style={{ fontSize: '16px', color: '#a3a5a9' }}
+                    type="dollar"
+                  />
+                </Tooltip>
+                1,156
+              </span>
+            </div>
+          </div>
+          <Divider />
+          <Label labelText="Upload Evidence" theme="LabelBlue" />
+          <DragUploadFile />
+          <TableEvidence />
+        </div>
+        <div className="ControlSteps StepOne">
+          <Button type="primary">Continue</Button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default ProjectEvidence;
--- pages/project-progress.jsx
+++ pages/project-progress.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, message, Divider, Button, Icon } from 'antd';
+import { Tabs, message, Divider, Button, Icon, Breadcrumb } from 'antd';
 import Header from '../components/molecules/Header/Header';
 import SideBar from '../components/organisms/SideBar/SideBar';
 import CustomButton from '../components/atoms/CustomButton/CustomButton';
@@ -8,21 +8,28 @@ import './_concensus.scss';
 import './_steps.scss';
 import TableProjectProgress from '../components/organisms/TableProjectProgress/TableProjectProgress';
 
+const BreadCrumb = () => (
+  <Breadcrumb>
+    <Breadcrumb.Item>
+      <a href="@">Back</a>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>Project Progress</Breadcrumb.Item>
+  </Breadcrumb>
+);
+
 const ProjectProgress = () => (
   <div className="AppContainer">
     <SideBar />
     <div className="MainContent">
       <Header />
       <div className="Content">
+        <BreadCrumb />
         <div className="ProjectInfoHeader">
           <div className="space-between">
-            <div className="">
-              <div>
-                <p className="LabelSteps">Project Name</p>
-                <h1>Nombre del Proyecto</h1>
-              </div>
+            <div>
+              <p className="LabelSteps">Project Name</p>
+              <h1>Women Professional Development</h1>
             </div>
-            <CustomButton buttonText="Start Project" theme="Primary" />
           </div>
         </div>
         <TableProjectProgress />
