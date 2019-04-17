7d1bf818c1e571027079956c15c4bdbffea35bf2
Federico Jure <fjure@atixlabs.com>
2019-04-17 12:18:03 -0300

    COA-479 allow fund only as Funder

--- components/atoms/CustomButton/CustomButton.jsx
+++ components/atoms/CustomButton/CustomButton.jsx
@@ -5,7 +5,7 @@ import { Button, Icon } from 'antd';
 const CustomButton = ({ id, theme, buttonText, onClick, icon, disabled }) => {
   const classname = `CustomButton ${theme}`;
   return (
-    <Button id={id} className={classname} onClick={onClick}>
+    <Button id={id} className={classname} onClick={onClick} disabled={disabled}>
       <span>{buttonText} </span>
       <Icon type={icon} />
     </Button>
