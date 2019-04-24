171eb3eb27061819fb2f3448e80ef37125d0b8c8
lgobbi <lgobbi@atixlabs.com>
2019-04-24 17:46:07 -0300

    COA-474 modify bo project detail content with no styles

--- pages/back-office-project-detail.jsx
+++ pages/back-office-project-detail.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Table } from 'antd';
 import { getProject } from '../api/projectApi';
+import { getPhoto } from '../api/photoApi';
 import Header from '../components/molecules/Header/Header';
 import SideBar from '../components/organisms/SideBar/SideBar';
+import ProjectMission from '../components/molecules/ProjectMission/ProjectMission';
+import GeneralItem from '../components/atoms/GeneralItem/GeneralItem';
 import Routing from '../components/utils/Routes';
 import CustomButton from '../components/atoms/CustomButton/CustomButton';
 import ProjectStatus from '../constants/ProjectStatus';
@@ -10,93 +12,102 @@ import ProjectStatus from '../constants/ProjectStatus';
 import './_style.scss';
 import './_back-office-projec-detail.scss';
 
-const columns = [
-  {
-    title: 'Project Name',
-    dataIndex: 'projectName',
-    key: 'projectName',
-    fixed: 'left'
-  },
-  {
-    title: 'Mission',
-    dataIndex: 'mission',
-    key: 'mission',
-    render: mission => (
-      <div style={{ whiteSpace: 'normal', wordWrap: 'break-all' }}>
-        {mission}
-      </div>
-    )
-  },
-  {
-    title: 'Problem Addressed',
-    dataIndex: 'problemAddressed',
-    key: 'problemAddressed',
-    render: problemAddressed => (
-      <div style={{ whiteSpace: 'normal', wordWrap: 'break-all' }}>
-        {problemAddressed}
-      </div>
-    )
-  },
-  {
-    title: 'Location',
-    dataIndex: 'location',
-    key: 'location'
-  },
-  {
-    title: 'Timeframe',
-    dataIndex: 'timeframe',
-    key: 'timeframe'
-  },
-  {
-    title: 'Goal Amount',
-    dataIndex: 'goalAmount',
-    key: 'goalAmount'
-  },
-  {
-    title: 'FAQ Link',
-    dataIndex: 'faqLink',
-    key: 'faqLink',
-    fixed: 'right',
-    render: faqLink => <a target="_blank" href={faqLink}>{faqLink}</a>
-  }
-];
-
 class BackofficeProjectDetail extends React.Component {
   static async getInitialProps({ query }) {
     const { projectId } = query;
     const project = (await getProject(projectId)).data;
-    return { projectId, project };
+    const coverPhoto = await getPhoto(project.coverPhoto);
+    const projectDetail = {
+      ...project,
+      coverPhoto: coverPhoto.data
+    };
+    return { projectDetail };
   }
 
   render() {
-    const { project } = this.props;
-    console.log(project);
+    const { projectDetail } = this.props;
+    console.log(projectDetail);
+    const itemsData = projectDetail
+      ? [
+          {
+            subtitle: 'Country of Impact',
+            title: projectDetail.location,
+            iconItem: 'environment'
+          },
+          {
+            subtitle: 'Timeframe',
+            title: projectDetail.timeframe,
+            iconItem: 'calendar'
+          },
+          {
+            subtitle: 'Amount',
+            title: projectDetail.goalAmount,
+            iconItem: 'dollar'
+          },
+          {
+            subtitle: 'Name of Lead',
+            title: projectDetail.ownerName,
+            iconItem: 'user'
+          },
+          {
+            subtitle: 'Mail of Lead',
+            title: projectDetail.ownerEmail,
+            iconItem: 'mail'
+          }
+        ]
+      : [];
     return (
       <div className="AppContainer">
         <SideBar />
         <div className="MainContent">
           <Header />
           <div className="TableContainer">
+            <div>
+              <img
+                src={projectDetail.coverPhoto || ''}
+                alt="projectCoverImage"
+              />
+              <div>
+                <p>Entreprise</p>
+                <h1>{projectDetail ? projectDetail.projectName : ''}</h1>
+              </div>
+            </div>
             <div className="HeaderProjectDetail">
               <img
                 src="./static/images/button-arrow-back.svg"
                 onClick={Routing.goBack}
+                alt="Go Back"
               />
               <h1>Project Details</h1>
             </div>
-            <Table
-              columns={columns}
-              dataSource={[project]}
-              scroll={{ x: 1500 }}
-            />
+            <div>
+              <ProjectMission
+                mission={projectDetail ? projectDetail.mission : ''}
+                terms={projectDetail ? projectDetail.problemAddressed : ''}
+              />
+              <div className="ProjectGeneralData">
+                <div className="block">
+                  <h1 className="title">Generals</h1>
+                </div>
 
-            {ProjectStatus.IN_PROGRESS === project.status ? (
+                {itemsData.map((item, i) => (
+                  <GeneralItem
+                    subtitle={item.subtitle}
+                    title={item.title}
+                    iconItem={item.iconItem}
+                    key={i}
+                  />
+                ))}
+              </div>
+            </div>
+
+            {ProjectStatus.IN_PROGRESS === projectDetail.status ? (
               <CustomButton
                 theme="Primary"
                 buttonText="View Progress"
                 onClick={() =>
                   Routing.toProjectProgress({
-                    projectId: project.id
+                    projectId: projectDetail.id
                   })
                 }
               />
