8d9d26b76946783f8b88578ac719bb67539fc90e
eugenia romero <eromero@atixlabs.com>
2019-05-22 18:50:04 +0000

    643-project progress bar and oracle

--- components/molecules/ProjectMission/ProjectMission.jsx
+++ components/molecules/ProjectMission/ProjectMission.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-import "./_style.scss";
+import React from 'react';
+import { Steps } from 'antd';
+import './_style.scss';
+
+const { Step } = Steps;
 
 const ProjectMission = ({ mission, terms }) => (
-
   <div className="ProjectMission">
     <div className="block">
-    <h1 className="title">Our Mission</h1>
-    <p>{mission}</p>
+      <h1 className="title">Our Mission</h1>
+      <p>{mission}</p>
     </div>
     <div className="block">
-    <h1 className="title">Problem Addressed</h1>
+      <h1 className="title">Problem Addressed</h1>
       <p>{terms}</p>
+    </div>
+    <div className="block">
+      <h1 className="title">Project Progress</h1>
+      <Steps size="small" current={1}>
+        <Step />
+        <Step title="USD 25K" />
+        <Step title="USD 10K" />
+      </Steps>
     </div>
   </div>
 );
