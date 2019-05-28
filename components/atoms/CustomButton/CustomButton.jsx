476d857d1cff74e060ee4c100b2d379475f3c1cb
federico <fjure@atixlabs.com>
2019-05-28 13:27:23 -0300

    COA-669 add htmltype on custom-buttons and onSubmit to login form

--- components/atoms/CustomButton/CustomButton.jsx
+++ components/atoms/CustomButton/CustomButton.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import './_style.scss';
 import { Button, Icon } from 'antd';
 
-const CustomButton = ({ id, theme, buttonText, onClick, icon, disabled }) => {
+const CustomButton = ({
+  id,
+  theme,
+  buttonText,
+  onClick,
+  icon,
+  disabled,
+  htmlType
+}) => {
   const classname = `CustomButton ${theme}`;
   return (
-    <Button id={id} className={classname} onClick={onClick} disabled={disabled}>
+    <Button
+      id={id}
+      className={classname}
+      onClick={onClick}
+      disabled={disabled}
+      htmlType={htmlType}
+    >
       <span>{buttonText} </span>
       <Icon type={icon} />
     </Button>
