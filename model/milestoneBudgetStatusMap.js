2756fe0bfd672faa6bcbd57b5d722185f4c73eb8
lgobbi <lgobbi@atixlabs.com>
2019-06-12 16:17:59 -0300

    COA-721 add license header to js and jsx files

--- model/milestoneBudgetStatusMap.js
+++ model/milestoneBudgetStatusMap.js
@@ -1,3 +1,11 @@
+/**
+ * COA PUBLIC LICENSE
+ * Circle of Angels aims to democratize social impact financing.
+ * It facilitate the investment process by utilizing smart contracts to develop impact milestones agreed upon by funders and the social entrepenuers.
+ *
+ * Copyright (C) 2019 AtixLabs, S.R.L <https://www.atixlabs.com>
+ */
+
 const MilestoneBudgetStatusMap = {
   '1': { name: 'Claimable', color: 'yellow' },
   '2': { name: 'Claimed', color: 'blue' },
