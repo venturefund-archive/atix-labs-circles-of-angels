421d7dbf6bea2245e0dc653e49a21c40788c646a
Leonel Gobbi <lgobbi@atixlabs.com>
2019-06-13 17:30:57 +0000

    COA-721 add license header to js and jsx files

--- model/userRegistrationStatusMap.js
+++ model/userRegistrationStatusMap.js
@@ -1,3 +1,11 @@
+/**
+ * AGPL License
+ * Circle of Angels aims to democratize social impact financing.
+ * It facilitate the investment process by utilizing smart contracts to develop impact milestones agreed upon by funders and the social entrepenuers.
+ *
+ * Copyright (C) 2019 AtixLabs, S.R.L <https://www.atixlabs.com>
+ */
+
 const userRegistrationStatusMap = {
   '1': { name: 'Pending approval', color: '' },
   '2': { name: 'Approved', color: 'green' },
