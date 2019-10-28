27b5b850da1a8f6ad43c6cc0178d5408e8a3a94b
Budi <ignacio.tirabasso@gmail.com>
2019-10-28 16:20:57 -0300

    Add role constants.

---
+++ constants/constants.js
@@ -0,0 +1,17 @@
+/**
+ * AGPL License
+ * Circle of Angels aims to democratize social impact financing.
+ * It facilitate the investment process by utilizing smart contracts to develop impact milestones agreed upon by funders and the social entrepenuers.
+ *
+ * Copyright (C) 2019 AtixLabs, S.R.L <https://www.atixlabs.com>
+ */
+
+export const ENTREPRENEUR = 'entrepreneur';
+export const FUNDER = 'funder';
+export const ORACLE = 'oracle';
+
+export const ROLE_LABELS = {
+  ENTREPRENEUR: 'Social Entrepreneur',
+  FUNDER: 'Impact funder',
+  ORACLE: 'Oracle'
+};
