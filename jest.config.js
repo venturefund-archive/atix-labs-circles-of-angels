02ba4f3a142b8a1b73e44db7a81aef1a98f3f876
Budi <ignacio.tirabasso@gmail.com>
2019-10-29 14:34:13 -0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,7 @@ module.exports = {
   ],
   moduleDirectories: [
     'node_modules',
-    'utils', // a utility folder
-    __dirname // the root directory
+    __dirname
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
