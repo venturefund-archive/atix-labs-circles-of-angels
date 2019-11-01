f989e9bb72487aa97b37ba8921b0bcff198064eb
Eugenia Romero <eugeromero-atixlabs@MacBook-Air-de-Eugenia-2.local>
2019-11-01 13:29:19 -0300

    929-create proposal page and footer component

---
+++ components/organisms/FooterButtons/FooterButtons.jsx
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react';
+import {
+  Tag,
+  Row,
+  Col,
+  Breadcrumb,
+  Divider,
+  Form,
+  Icon,
+  Input,
+  Upload
+} from 'antd';
+import CustomButton from '../../atoms/CustomButton/CustomButton';
+import './_style.scss';
+
+const FooterButtons = () => (
+    <Row
+    className="FooterButtons"
+    type="flex"
+    justify="space-around"
+    align="middle"
+    >
+    <Col
+    xs={{ span: 24, order: 1 }}
+    sm={{ span: 24, order: 1 }}
+    md={6}
+    lg={{ span: 4, offset: 0, order: 1 }}
+    >
+    <CustomButton buttonText="Back" theme="Cancel" />
+    </Col>
+
+    <Col
+    className="space-between"
+    xs={{ span: 24, order: 2 }}
+    sm={{ span: 24, order: 2 }}
+    md={6}
+    lg={{ span: 3, offset: 15, order: 3 }}
+    >
+    <CustomButton
+        buttonText="Save & Continue"
+        theme="Primary"
+        classNameIcon="iconDisplay"
+        icon="arrow-right"
+    />
+    </Col>
+    </Row>
+)
+export default FooterButtons;
