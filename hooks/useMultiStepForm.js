174d29a3a9d9d146e21aa3936028e6209971b379
upadilla@atixlabs.com <upadilla@atixlabs.com>
2019-12-19 13:32:28 -0300

    985 - Message added when some form is incompleted.

--- hooks/useMultiStepForm.js
+++ hooks/useMultiStepForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { message } from 'antd';
 import CustomButton from '../components/atoms/CustomButton/CustomButton';
 
 export default function useMultiStepForm(
@@ -120,7 +121,10 @@ export default function useMultiStepForm(
     const last = isLast(currentStep);
     const isValid = validateFields(e);
 
-    if (!isValid) return;
+    if (!isValid) {
+      message.error('Complete all the fields');
+      return;
+    }
 
     if (last) setShouldSubmit(true);
     else setCurrentStep(currentStep + 1);
