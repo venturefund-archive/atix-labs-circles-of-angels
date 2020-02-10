55d618e082bab98ac5baf05f9fb2faa46157512f
upadilla@atixlabs.com <upadilla@atixlabs.com>
2020-02-10 09:45:23 -0300

    feat: Fund administration page updated to new fields
    Refs #1219

--- components/organisms/TableAdmin/TableAdminTransfers.jsx
+++ components/organisms/TableAdmin/TableAdminTransfers.jsx
@@ -6,40 +6,32 @@
  * Copyright (C) 2019 AtixLabs, S.R.L <https://www.atixlabs.com>
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './_style.scss';
-import { Table } from 'antd';
-import CustomButton from '../../atoms/CustomButton/CustomButton';
-import transferStatusMap from '../../../model/transferStatus';
+import { Table, Tag } from 'antd';
+import transferStatusesMap from '../../../model/transferStatusesMap';
 
-class TableAdminTransfers extends React.Component {
-  constructor(props) {
-    super(props);
+const TableAdminTransfers = ({ projectId, getTransfers }) => {
+  const [transfers, setTransfers] = useState([]);
 
-    this.state = {
-      transfers: []
-    };
-  }
-
-  componentDidMount = () => this.fetchTransfers();
-
-  fetchTransfers = async () => {
-    const { projectId, getTransfers } = this.props;
-    const data = await getTransfers(projectId);
-    this.setState({ transfers: data || [] });
-  };
-
-  getColumns = () => [
+  const columns = [
     {
       title: 'Transfer Id',
-      dataIndex: 'id',
-      key: 'id'
+      dataIndex: 'transferId',
+      key: 'transferId'
     },
     {
-      title: 'User Id',
-      dataIndex: 'sender',
-      key: 'name'
+      title: 'Sender',
+      key: 'sender',
+      render: ({ sender }) => (
+        <span>{`${sender.firstName} ${sender.lastName}`}</span>
+      )
+    },
+    {
+      title: 'Destination Account',
+      dataIndex: 'destinationAccount',
+      key: 'destinationAccount'
     },
     {
       title: 'Amount',
@@ -47,68 +39,58 @@ class TableAdminTransfers extends React.Component {
       key: 'amount'
     },
     {
-      title: 'Receipt Number',
-      dataIndex: 'transferId',
-      key: 'transferId'
+      title: 'Currency',
+      dataIndex: 'currency',
+      key: 'currency'
+    },
+    {
+      title: 'Receipt',
+      dataIndex: 'receiptPath',
+      key: 'receiptPath',
+      // TODO check how we will handle this
+      render: receipt => (
+        <a href={receipt} target="_blank" rel="noopener noreferrer">
+          View
+        </a>
+      )
     },
     {
       title: 'Status',
+      key: 'status',
       dataIndex: 'status',
-      key: 'status'
-      // TODO Disabled until functionality is defined
-      // render: (text, record) => (
-      //   <span>
-      //     <select
-      //       onChange={evnt => (record.status = evnt.currentTarget.value)}
-      //       defaultValue={record.status}
-      //     >
-      //       {Object.keys(transferStatusMap).map(transferStatusKey => (
-      //         <option key={transferStatusKey} value={transferStatusKey}>
-      //           {transferStatusMap[transferStatusKey].show}
-      //         </option>
-      //       ))}
-      //     </select>
-      //   </span>
-      // )
+      render: status => (
+        <span>
+          <Tag color={transferStatusesMap[status].color} key={status}>
+            {transferStatusesMap[status].name}
+          </Tag>
+        </span>
+      )
     }
-    // TODO Disabled until functionality is defined
-    // {
-    //   title: 'Action',
-    //   key: 'action',
-    //   render: (text, record) => (
-    //     <span>
-    //       <CustomButton
-    //         theme="Primary"
-    //         buttonText="Confirm"
-    //         onClick={() => {
-    //           console.log(record);
-    //           this.props.saveStatus(record.id, record.state);
-    //         }}
-    //       />
-    //     </span>
-    //   )
-    // }
   ];
 
-  render() {
-    const { transfers } = this.state;
+  const fetchTransfers = async () => {
+    const data = await getTransfers(projectId);
+    setTransfers(data);
+  };
 
-    return (
-      <Table
-        columns={this.getColumns()}
-        dataSource={transfers}
-        size="middle"
-        className="TableAdmin"
-        pagination={false}
-      />
-    );
-  }
-}
+  useEffect(() => {
+    fetchTransfers();
+  }, []);
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={transfers}
+      size="middle"
+      className="TableAdmin"
+      pagination={false}
+    />
+  );
+};
 
 export default TableAdminTransfers;
 
 TableAdminTransfers.propTypes = {
   projectId: PropTypes.number.isRequired,
-  saveStatus: PropTypes.func.isRequired,
   getTransfers: PropTypes.func.isRequired
 };
