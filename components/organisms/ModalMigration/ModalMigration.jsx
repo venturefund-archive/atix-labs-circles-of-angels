7af9f8e88c2a4ad5240de6e2eca5693c52533f99
Eugenia Romero <eugeromero-atixlabs@MacBook-Air-de-Eugenia-2.local>
2020-02-27 15:07:19 -0300

    1274-add projects img

--- components/organisms/ModalMigration/ModalMigration.jsx
+++ components/organisms/ModalMigration/ModalMigration.jsx
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, message } from 'antd';
+import { Modal, Col, Row } from 'antd';
 import './_style.scss';
 import { useHistory } from 'react-router';
 import { useUserContext } from '../../utils/UserContext';
@@ -55,12 +55,40 @@ class ModalMigration extends React.Component {
         >
         <div className="Container">
           <div className="Body">
-                    <h1>Hello!</h1>
-                    <h2>Are you looking for any of these projects?</h2>
-                    <h3>We are migrating to a new platform and some projects have not been added yet!
-                    If your project is on this list, enter to the previous platform from the button to access them
-                    Otherwise continue to the landing page
-                    </h3>
+                    <div>
+                      <h1>Hello!</h1>
+                      <h2>Are you looking for any of these projects?</h2>
+                      <h3>We are migrating to a new platform and some projects have not been added yet!
+                      If your project is on this list, enter to the previous platform from the button to access them
+                      Otherwise continue to the landing page
+                      </h3>
+                     </div>
+                    <Row className="BlockContainer" gutter={16}>
+                      <Col span={6} className="Card">
+                        <div className="ImageCard">
+                         <img src="/static/images/sehatkahani.jpg" />
+                        </div>
+                          <h1>Sehat Kahani</h1>
+                      </Col>
+                      <Col span={6} className="Card">
+                        <div span={24} className="ImageCard">
+                        <img src="/static/images/dengue.jpg" />
+                        </div>
+                          <h1>Dengue Prognostic Test</h1>
+                      </Col>
+                      <Col span={6} className="Card">
+                        <div span={24} className="ImageCard">
+                        <img src="/static/images/wedu.jpg" />
+                        </div>
+                          <h1>Wedu Global Fisa</h1>
+                      </Col>
+                      <Col span={6} className="Card">
+                        <div span={24} className="ImageCard">
+                        <img src="/static/images/hammock.jpg" />
+                        </div>
+                          <h1>Yellow Leaf Hammock</h1>
+                      </Col>
+                    </Row>
           </div>
           <div className="Footer">
             <CustomButton theme="Primary" buttonText="Yes, go to the old platform!"/>
