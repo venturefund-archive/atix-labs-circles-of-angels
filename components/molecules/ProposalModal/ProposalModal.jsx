7706b5eb9b9f34cc81d20eabf0286352f9de8f5e
Federico Jure <fjure@atixlabs.com>
2020-05-19 16:58:16 -0300

    feat: create modal

--- components/molecules/ProposalModal/ProposalModal.jsx
+++ components/molecules/ProposalModal/ProposalModal.jsx
@@ -7,9 +7,14 @@
  */
 
 import React, { useState } from 'react';
-import { Modal, Input } from 'antd';
+import { Modal, Input, Collapse, Popconfirm, message } from 'antd';
 import CustomButton from '../../atoms/CustomButton/CustomButton';
+import ModalMemberSelection from '../../molecules/ModalMemberSelection/ModalMemberSelection';
 import './_style.scss';
+
+function callback(key) {
+  console.log(key);
+}
 
 function ProposalModal() {
   const [visible, setVisible] = useState(false);
@@ -30,6 +35,8 @@ function ProposalModal() {
 
   const { TextArea } = Input;
 
+  const { Panel } = Collapse;
+
   return (
     <div>
       <CustomButton
@@ -43,8 +50,84 @@ function ProposalModal() {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Input placeholder="Add a description" />
+        <Input className="inputPriary" placeholder="Add a description" />
         <TextArea rows={4} />
+        <Collapse onChange={callback}>
+          <Panel
+            header={
+              <div className="flex categoryCollapseHeader">
+                <p>Category</p>
+                <p className="categorySelected">-None</p>{' '}
+                <CustomButton theme="Category" buttonText="Select Type" />
+              </div>
+            }
+            key="1"
+          >
+            <p className="CollapseInnerTitle">New Member</p>
+            <div className="daoRoleContainer flex">
+              <img src="../static/images/icon-modal-01.png" />
+              <div className="column">
+                <p>
+                  <strong>NEW MEMBER</strong>
+                </p>
+                <p>Lorem ipsum.</p>
+              </div>
+            </div>
+
+            <ModalMemberSelection />
+
+            <p className="CollapseInnerTitle">New Rol</p>
+            <div className="daoRoleContainer flex">
+              <img src="../static/images/icon-modal-02.png" />
+              <div className="column">
+                <p>
+                  <strong>NEW ROL</strong>
+                </p>
+                <p>Lorem ipsum.</p>
+              </div>
+            </div>
+            <p className="CollapseInnerTitle">Create DAO</p>
+            <div className="daoRoleContainer flex">
+              <img src="../static/images/icon-modal-03.png" />
+              <div className="column">
+                <p>
+                  <strong>CREATE DAO</strong>
+                </p>
+                <p>Lorem ipsum.</p>
+              </div>
+            </div>
+          </Panel>
+        </Collapse>
+        <div className="flex space-between">
+          <div className="column">
+            <p>
+              <strong>Set deadline</strong>
+            </p>
+            <p>How soon are you looking for responses?</p>
+          </div>
+          <div className="space-between">
+            <Popconfirm
+              title={
+                <div className="column">
+                  <p>Today (2 hours)</p>
+                  <p>Tomorroy (24 hours)</p>
+                  <p>This Week (7 days)</p>
+                </div>
+              }
+            >
+              <div className="flex date">
+                <img src="../static/images/icon-time-orange.png" />
+                <p>Due date: 7 days</p>
+              </div>
+            </Popconfirm>
+            ,
+            <img src="../static/images/icon-pencil.png" />
+          </div>
+        </div>
+        <div className="flex space-between border-top margin-top padding-top">
+        <CustomButton theme="Alternative" buttonText="Cancel" />
+          <CustomButton theme="Primary" buttonText="Ok" />
+        </div>
       </Modal>
     </div>
   );
