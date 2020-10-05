c7356ad4a1ae7270d9698110c4059ad24138196d
gcondori <gcondori@atixlabs.com>
2020-10-05 09:32:17 -0300

    fix: show fund transfers with status rejected

--- model/transferStatusesMap.js
+++ model/transferStatusesMap.js
@@ -3,7 +3,8 @@ const transferStatusesMap = {
   reconciliation: { name: 'Reconciliation', color: 'yellow' },
   verified: { name: 'Verified', color: 'green' },
   cancelled: { name: 'Cancelled', color: 'red' },
-  failed: { name: 'Failed', color: 'red' }
+  failed: { name: 'Failed', color: 'red' },
+  rejected: { name: 'Rejected', color: 'red' }
 };
 
 export default transferStatusesMap;
