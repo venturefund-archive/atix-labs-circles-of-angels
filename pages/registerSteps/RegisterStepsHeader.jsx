80d044738f6e57346e6a2286e1429ff410a0483e
Tomas Hurrell <thurrell@atixlabs.com>
2020-10-09 17:50:38 -0300

    COA-1461-fix: fixed login button in register views to open the login modal

--- pages/registerSteps/RegisterStepsHeader.jsx
+++ pages/registerSteps/RegisterStepsHeader.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col } from 'antd';
+import ModalLogin from '../../components/organisms/ModalLogin/ModalLogin';
+import CustomButton from '../../components/atoms/CustomButton/CustomButton';
 
-const RegisterStepsHeader = () => (
-  <Row className="TopBar" type="flex" justify="space-between" align="middle">
-    <Col className="gutter-row" xs={10} sm={4} lg={4}>
-      <img src="./static/images/icon-large.svg" alt="Circles of Angels" />
-    </Col>
-    <Col
-      className="gutter-row"
-      xs={12}
-      sm={{ span: 7, offset: 10 }}
-      lg={{ span: 4, offset: 13 }}
-    >
-      Already Registered? <a href="/landing">Log In</a>
-    </Col>
-  </Row>
-);
+const RegisterStepsHeader = () => {
+  const [visibility, setVisibility] = useState(false);
+
+  return (
+    <Row className="TopBar" type="flex" justify="space-between" align="middle">
+      <Col className="gutter-row" xs={10} sm={4} lg={4}>
+        <img src="./static/images/icon-large.svg" alt="Circles of Angels" />
+      </Col>
+      <Col
+        className="gutter-row"
+        xs={12}
+        sm={{ span: 7, offset: 10 }}
+        lg={{ span: 4, offset: 13 }}
+      >
+        Already Registered?
+        <div className="WrapperModalLogin">
+          <CustomButton
+            data-testid="loginButton"
+            buttonText="Login"
+            theme="Secondary"
+            onClick={() => setVisibility(true)}
+          />
+          <ModalLogin
+            data-testid="modal"
+            setVisibility={setVisibility}
+            visibility={visibility}
+          />
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 export default RegisterStepsHeader;
