ac63e2b0d1c26d59a2f8722af163abfa0fbf16d1
German Condori <gcondori@atixlabs.com>
2020-11-16 14:15:39 +0000

    COA-1712 - fix: add pending_verification status

--- model/transferStatusesMap.js
+++ model/transferStatusesMap.js
@@ -4,7 +4,8 @@ const transferStatusesMap = {
   verified: { name: 'Verified', color: 'green' },
   cancelled: { name: 'Cancelled', color: 'red' },
   failed: { name: 'Failed', color: 'red' },
-  rejected: { name: 'Rejected', color: 'red' }
+  rejected: { name: 'Rejected', color: 'red' },
+  pending_verification: { name: 'Pending verification', color: 'yellow' },
 };
 
 export default transferStatusesMap;
