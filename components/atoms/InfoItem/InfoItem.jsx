af55349e7d7822fdca896d963cf723394e639362
Tomas Hurrell <thurrell@atixlabs.com>
2020-11-19 18:08:58 -0300

    fix: fixed countries labels moving the layout. Now it overflows with ellipsis and applies a Tooltip dinamicly to see all the content

--- components/atoms/InfoItem/InfoItem.jsx
+++ components/atoms/InfoItem/InfoItem.jsx
@@ -6,22 +6,48 @@
  * Copyright (C) 2019 AtixLabs, S.R.L <https://www.atixlabs.com>
  */
 
-import React from 'react';
-import { Icon, Col, Row } from 'antd';
+import React, { useEffect, useState, useRef } from 'react';
+import { Col, Tooltip } from 'antd';
 import './_style.scss';
 
-const InfoItem = ({ subtitle, title, img, xs, sm , lg }) => (
-  <Col 
-  xs={xs}
-  sm={sm}
-  lg={lg}
-   className="InfoItem">
-    <div className="InfoItemData">
-      <p>{subtitle}</p>
-      <h2>{title}</h2>
-      {img}
-    </div>
-  </Col>
-);
+const InfoItem = ({ subtitle, title, img, xs, sm, lg }) => {
+  const [overflowTooltip, setOverflowTooltip] = useState(false);
+
+  const titleRef = useRef();
+
+  const isTextOverflow = element =>
+    element && element.clientWidth < element.scrollWidth;
+
+  const checkOverflow = () => {
+    const element = titleRef.current;
+
+    const overflow = isTextOverflow(element);
+    if (overflow !== overflowTooltip) {
+      setOverflowTooltip(overflow);
+    }
+  };
+
+  useEffect(() => {
+    checkOverflow();
+  });
+
+  const h2Title = () => <h2 ref={titleRef}>{title}</h2>;
+
+  return (
+    <Col xs={xs} sm={sm} lg={lg} className="InfoItem">
+      <div className="InfoItemData">
+        <p>{subtitle}</p>
+        {overflowTooltip ? (
+          <Tooltip placement="bottom" title={title}>
+            {h2Title()}
+          </Tooltip>
+        ) : (
+          h2Title()
+        )}
+        {img}
+      </div>
+    </Col>
+  );
+};
 
 export default InfoItem;
