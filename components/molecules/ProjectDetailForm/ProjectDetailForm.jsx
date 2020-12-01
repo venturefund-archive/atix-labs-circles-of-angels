53f0d155319013d863be896aae6fca3aad427408
Tomas Hurrell <thurrell@atixlabs.com>
2020-12-01 16:34:40 -0300

    fix: fixed upload buttons when long filename is selected

--- components/molecules/ProjectDetailForm/ProjectDetailForm.jsx
+++ components/molecules/ProjectDetailForm/ProjectDetailForm.jsx
@@ -32,141 +32,136 @@ const ProjectDetailForm = ({ thumbnailsData, fields, handleChange }) => {
     loadPhotoPreview();
   }, [fields.coverPhotoPath.value]);
 
-  return (
-    <Fragment>
-      <Row type="flex" justify="space-around" className="centered">
-        <Col className="AlignCenter" sm={8} md={8} lg={8}>
-          <Col className="CardExample" sm={24} md={24} lg={24}>
-            <Col className="BlockImage" sm={24} md={24} lg={24}>
-              <h5>Organization Name</h5>
-              <h1>{getPreviewValue(thumbnailsData.projectName)}</h1>
+  const ProjectDetailPreview = (
+    <Col className="AlignCenter" span={8}>
+      <Col className="CardExample" span={24}>
+        <Col className="BlockImage" span={24}>
+          <h5>Organization Name</h5>
+          <h1>{getPreviewValue(thumbnailsData.projectName)}</h1>
 
-              <img
-                className="BackgroundImage"
-                width="700"
-                height="400"
-                src={photoPreview || './static/images/thumbnail-example.png'}
-                alt="background"
+          <img
+            className="BackgroundImage"
+            width="700"
+            height="400"
+            src={photoPreview || './static/images/thumbnail-example.png'}
+            alt="background"
+          />
+
+          <Col className="flex" span={24}>
+            <InfoItem
+              img={
+                <img src="./static/images/world.svg" alt="Circles of Angels" />
+              }
+              subtitle="Country of Impact"
+              title={getPreviewValue(thumbnailsData.location)}
+              iconInfoItem="dollar"
+            />
+            <InfoItem
+              img={
+                <img
+                  src="./static/images/calendar.svg"
+                  alt="Circles of Angels"
+                />
+              }
+              subtitle="Timeframe"
+              title={getPreviewValue(thumbnailsData.timeframe)}
+              iconInfoItem="dollar"
+            />
+            <InfoItem
+              img={
+                <img src="./static/images/amount.svg" alt="Circles of Angels" />
+              }
+              subtitle="Goal Amount"
+              title={`$ ${getPreviewValue(thumbnailsData.goalAmount)}`}
+              iconInfoItem="dollar"
+            />
+          </Col>
+        </Col>
+        <Col className="spacedivider Details" span={24}>
+          <Col span={24}>
+            <h4>Project Mission </h4>
+          </Col>
+          <Col className="Mission" span={24}>
+            {fields.mission.value || <Skeleton title={false} />}
+          </Col>
+          <Col span={24}>
+            <h4>The Problem </h4>
+          </Col>
+          <Col className="Problem" span={24}>
+            {fields.problemAddressed.value || (
+              <Skeleton paragraph={{ rows: 2 }} title={false} />
+            )}
+          </Col>
+        </Col>
+      </Col>
+    </Col>
+  );
+
+  const DetailForm = (
+    <Col className="AlignCenter" span={12}>
+      <Col span={24} className="BlockForm">
+        <Row gutter={22}>
+          <Col className="InputTwoLabel" span={24}>
+            <Field {...fields.mission} handleChange={handleChange} />
+          </Col>
+          <Col className="InputTwoLabel" span={24}>
+            <Field {...fields.problemAddressed} handleChange={handleChange} />
+          </Col>
+          <Col className="space-between">
+            <div className="upload-info">
+              <h3>Background Image</h3>
+              <span>
+                Recommended Image Size: 1400x400px.Format: PNG or JPG.
+              </span>
+            </div>
+            <div className="upload-button-field">
+              <Field
+                {...fields.coverPhotoPath}
+                handleChange={handleChange}
+                showPreviouslyUploadedList
+                isImage
               />
+            </div>
+          </Col>
+          <Col className="space-between">
+            <div className="upload-info">
+              <h3>Legal Agreement</h3>
+              <span>Format: PDF, DOC or DOCX</span>
+            </div>
+            <div className="upload-button-field">
+              <Field
+                {...fields.agreementFile}
+                handleChange={handleChange}
+                showPreviouslyUploadedList
+                label="Legal Agreement"
+              />
+            </div>
+          </Col>
+          <Col className="space-between">
+            <div className="upload-info">
+              <h3>Project Proposal</h3>
+              <span>Format: PDF, DOC or DOCX</span>
+            </div>
+            <div className="upload-button-field">
+              <Field
+                {...fields.proposalFile}
+                handleChange={handleChange}
+                showPreviouslyUploadedList
+                label="Project Proposal"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Col>
+  );
 
-              <Col className="flex" sm={24} md={24} lg={24}>
-                <InfoItem
-                  img={
-                    <img
-                      src="./static/images/world.svg"
-                      alt="Circles of Angels"
-                    />
-                  }
-                  subtitle="Country of Impact"
-                  title={getPreviewValue(thumbnailsData.location)}
-                  iconInfoItem="dollar"
-                />
-                <InfoItem
-                  img={
-                    <img
-                      src="./static/images/calendar.svg"
-                      alt="Circles of Angels"
-                    />
-                  }
-                  subtitle="Timeframe"
-                  title={getPreviewValue(thumbnailsData.timeframe)}
-                  iconInfoItem="dollar"
-                />
-                <InfoItem
-                  img={
-                    <img
-                      src="./static/images/amount.svg"
-                      alt="Circles of Angels"
-                    />
-                  }
-                  subtitle="Goal Amount"
-                  title={`$ ${getPreviewValue(thumbnailsData.goalAmount)}`}
-                  iconInfoItem="dollar"
-                />
-              </Col>
-            </Col>
-            <Col className="spacedivider Details" sm={24} md={24} lg={24}>
-              <Col sm={24} md={24} lg={24}>
-                <h4>Project Mission </h4>
-              </Col>
-              <Col className="Mission" sm={24} md={24} lg={24}>
-                {fields.mission.value || <Skeleton title={false} />}
-              </Col>
-              <Col sm={24} md={24} lg={24}>
-                <h4>The Problem </h4>
-              </Col>
-              <Col className="Problem" sm={24} md={24} lg={24}>
-                {fields.problemAddressed.value || (
-                  <Skeleton paragraph={{ rows: 2 }} title={false} />
-                )}
-              </Col>
-            </Col>
-          </Col>
-        </Col>
-        <Divider type="vertical" />
-        <Col className="AlignCenter" sm={12} md={12} lg={12}>
-          <Col sm={24} md={24} lg={24} className="BlockForm">
-            <Row gutter={22}>
-              <Form>
-                <Col className="InputTwoLabel" sm={24} md={24} lg={24}>
-                  <Field {...fields.mission} handleChange={handleChange} />
-                </Col>
-                <Col className="InputTwoLabel" sm={24} md={24} lg={24}>
-                  <Field
-                    {...fields.problemAddressed}
-                    handleChange={handleChange}
-                  />
-                </Col>
-                <Col sm={24} md={24} lg={24} className="space-between">
-                  <div className="upload-info">
-                    <h3>Background Image</h3>
-                    <span>
-                      Recomended Image Size: 1400x400px. Format: PNG or JPG.
-                    </span>
-                  </div>
-                  <div className="upload-button-field">
-                    <Field
-                      {...fields.coverPhotoPath}
-                      handleChange={handleChange}
-                      showPreviouslyUploadedList
-                      isImage
-                    />
-                  </div>
-                </Col>
-                <Col sm={24} md={24} lg={24} className="space-between">
-                  <div className="upload-info">
-                    <h3>Legal Agreement</h3>
-                    <span>Format: PDF, DOC or DOCX</span>
-                  </div>
-                  <div className="upload-button">
-                    <Field
-                      {...fields.agreementFile}
-                      handleChange={handleChange}
-                      showPreviouslyUploadedList
-                      label="Legal Agreement"
-                    />
-                  </div>
-                </Col>
-                <Col sm={24} md={24} lg={24} className="space-between">
-                  <div className="upload-info">
-                    <h3>Project Proposal</h3>
-                    <span>Format: PDF, DOC or DOCX</span>
-                  </div>
-                  <div className="upload-button">
-                    <Field
-                      {...fields.proposalFile}
-                      handleChange={handleChange}
-                      showPreviouslyUploadedList
-                      label="Project Proposal"
-                    />
-                  </div>
-                </Col>
-              </Form>
-            </Row>
-          </Col>
-        </Col>
-      </Row>
-    </Fragment>
+  return (
+    <Row type="flex" justify="space-around" className="centered">
+      {ProjectDetailPreview}
+      <Divider type="vertical" />
+      {DetailForm}
+    </Row>
   );
 };
 
