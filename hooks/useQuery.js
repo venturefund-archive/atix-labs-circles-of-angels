c15350faf9a96ecf3cfe1772385d81f11721a962
gcondori <gcondori@atixlabs.com>
2021-03-11 09:36:48 -0300

    fix: get uuid params form url

--- hooks/useQuery.js
+++ hooks/useQuery.js
@@ -1,15 +1,25 @@
 import { useLocation } from 'react-router-dom';
+import { isUuid } from 'uuidv4';
 
 function parseQuery(search) {
+  let object = {};
+  let uuid;
+  if (search.includes('id=')) {
+    const value = search.slice(
+      search.lastIndexOf('id=') + 3,
+      search.lastIndexOf('id=') + 39
+    );
+    uuid = isUuid(value) ? value : undefined;
+  }
   const params = new URLSearchParams(search);
   const entries = params.entries();
-  const object = {};
-  // eslint-disable-next-line no-restricted-syntax
-  for (const [key, value] of entries) {
-    object[key] = JSON.parse(value);
+  if (entries.length) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of entries) {
+      object[key] = JSON.parse(value);
+    }
   }
-
-  return object;
+  return uuid ? { id: uuid, ...object } : object;
 }
 
 export default function useQuery() {
