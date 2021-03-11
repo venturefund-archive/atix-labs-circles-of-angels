158f6874aee3d5933309f3fd661d6e4cf93b495e
gcondori <gcondori@atixlabs.com>
2021-03-11 16:22:28 -0300

    fix: use query-string to parse query values

--- hooks/useQuery.js
+++ hooks/useQuery.js
@@ -1,25 +1,8 @@
 import { useLocation } from 'react-router-dom';
-import { isUuid } from 'uuidv4';
+import queryString from 'query-string';
 
 function parseQuery(search) {
-  let object = {};
-  let uuid;
-  if (search.includes('id=')) {
-    const value = search.slice(
-      search.lastIndexOf('id=') + 3,
-      search.lastIndexOf('id=') + 39
-    );
-    uuid = isUuid(value) ? value : undefined;
-  }
-  const params = new URLSearchParams(search);
-  const entries = params.entries();
-  if (entries.length) {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const [key, value] of entries) {
-      object[key] = JSON.parse(value);
-    }
-  }
-  return uuid ? { id: uuid, ...object } : object;
+  return queryString.parse(search);
 }
 
 export default function useQuery() {
