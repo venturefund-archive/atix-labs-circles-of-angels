2491bc10e59a896f6e881d4b62d117927ab82602
diego.aguirre02 <diego.aguirre02@globant.com>
2022-12-12 16:12:02 -0500

    refactor(project info section): :recycle: correct eslint errors

--- components/organisms/ProjectInfoSection/ProjectInfoSection.jsx
+++ components/organisms/ProjectInfoSection/ProjectInfoSection.jsx
@@ -45,7 +45,12 @@ export const ProjectInfoSection = ({
 
 ProjectInfoSection.propTypes = {
   about: PropTypes.string,
-  mission: PropTypes.string
+  mission: PropTypes.string,
+  progressCurrentValue: PropTypes.number,
+  progressTotalValue: PropTypes.number,
+  balanceCurrentValue: PropTypes.number,
+  balanceTotalValue: PropTypes.number,
+  currency: PropTypes.string
 };
 
 ProjectInfoSection.defaultProps = {
@@ -53,8 +58,11 @@ ProjectInfoSection.defaultProps = {
     'Having spent a lot of time volunteering for different charities across East Africa since 2008, Jacqueline got inspired by the many kids she has met over the years to launch Shule. The foundation’s ultimate goal is to bring quality education to boys who are denied this fundamental human right. \n\n Many of the children she meets on the streets of Uganda are very intelligent, despite having little to no access to education due to their family’s economic status. Resulting in their potential often being unfulfilled.',
   mission:
     'Our mission is to empower families + communities through job creation. Each handwoven Yellow Leaf Hammock is created by an artisan from the hill-tribe communities of rural Thailand. \n\n By creating safe, high-wage weaving jobs, we divert families from toxic slash + burn agriculture and end the cycle of debt slavery. \n\n Good jobs empower + transform communities for generations.',
-  spent: 0,
-  budget: 0
+  progressCurrentValue: 0,
+  progressTotalValue: 0,
+  balanceCurrentValue: 0,
+  balanceTotalValue: 0,
+  currency: undefined
 };
 
 HorizontalBlockText.propTypes = {
