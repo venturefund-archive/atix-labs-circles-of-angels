fbdcefe14590a6a0ca48b773ccfe07b8c8ed1a08
Luis Fernando Cruces Vidal <luis.cruces@globant.com>
2022-12-13 23:40:22 -0500

    feat: add address to project member component

--- components/molecules/CoaProjectMembersCard/CoaProjectMembersCard.jsx
+++ components/molecules/CoaProjectMembersCard/CoaProjectMembersCard.jsx
@@ -1,27 +1,29 @@
 import { Avatar, Collapse } from 'antd';
 import React from 'react';
 import './coa-project-members-card.scss';
+import { Link } from 'react-router-dom';
+import { stringToHexColor } from '../../../helpers/stringToHexColor';
 
 const { Panel } = Collapse;
 
 const CustomCollapseHeader = ({ firstName, lastName, rol }) => {
   const customAvatarName = firstName.charAt(0).toUpperCase() + lastName.charAt(0).toUpperCase();
-  const randomAvatarColor = `#${((1<<24)*Math.random()|0).toString(16)}`;
-  const name = `${firstName} ${lastName}`;
+  const fullName = `${firstName} ${lastName}`;
+  const avatarColor = stringToHexColor(fullName);
   return (
     <div className='m-coaProjectMembersCard__header'>
-      <Avatar style={{ backgroundColor: randomAvatarColor }} size="large">
+      <Avatar style={{ backgroundColor: avatarColor }} size="large">
         {customAvatarName}
       </Avatar>
       <div style={{ flex: 1, minWidth: 0 }}>
-        <h5 className='m-coaProjectMembersCard__title'>{name}</h5>
+        <h5 className='m-coaProjectMembersCard__title'>{fullName}</h5>
         <h6 className='m-coaProjectMembersCard__subtitle'>{rol}</h6>
       </div>
     </div>
   );
 };
 
-export const CoaProjectMembersCard = ({ id, firstName, lastName, email, rol }) => (
+export const CoaProjectMembersCard = ({ id, firstName, lastName, email, rol, address }) => (
   <Collapse bordered={false} expandIconPosition='right' className='m-coaProjectMembersCard'>
     <Panel
       header={
@@ -35,9 +37,15 @@ export const CoaProjectMembersCard = ({ id, firstName, lastName, email, rol }) =
           <span className='m-coaProjectMembersCard__boldText'>Email: </span>
           {email}
         </div>
-        <div>
-          <span className='m-coaProjectMembersCard__boldText'>Address: </span>
-            0x3f427D8c5c1f48Fe6B65F80bBae8a49519E29316
+        <div className="m-coaProjectMembersCard__addressContainer">
+          <span className='m-coaProjectMembersCard__boldText'>Address:&nbsp;</span>
+          <Link
+              to={{ pathname: `https://etherscan.io/address/${address}` }}
+              target="_blank"
+              className="m-coaProjectMembersCard__addressContainer__link"
+          >
+            {address}
+          </Link>
         </div>
       </div>
     </Panel>
@@ -50,4 +58,5 @@ CoaProjectMembersCard.defaultProps = {
   lastName: undefined,
   email: undefined,
   rol: undefined,
+  address: undefined,
 };
