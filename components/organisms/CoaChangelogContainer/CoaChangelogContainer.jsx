faa3fa3b706b507dff659a77d7c63291aa5990c6
diego.aguirre02 <diego.aguirre02@globant.com>
2022-12-21 17:55:26 -0500

    refactor(evidences and changelog component): :recycle: correct styles & add empty state for evidences and changelog Container

--- components/organisms/CoaChangelogContainer/CoaChangelogContainer.jsx
+++ components/organisms/CoaChangelogContainer/CoaChangelogContainer.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './coa-changelog-container.scss';
 import { Divider } from 'antd';
 import { CoaTextButton } from 'components/atoms/CoaTextButton/CoaTextButton';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-export const CoaChangelogContainer = () => {
+export const CoaChangelogContainer = ({ title, emptyText }) => {
+  const [changeLogs, setChangeLogs] = useState([]);
   return (
     <div className="o-coaChangelogContainer">
       <div className="o-coaChangelogContainer__header">
-        <h3>Project Changelog</h3>
+        <h3>{title}</h3>
         <CoaTextButton>Download</CoaTextButton>
       </div>
       <Divider type="horizontal" />
+      <div
+        className={classNames('o-coaChangelogContainer__body', {
+          '--empty': changeLogs.length === 0
+        })}
+      >
+        {changeLogs.length === 0 && (
+          <>
+            <img src="/static/images/window-icon.png" alt="empty-changelog-list" />
+            <p className="o-coaChangelogContainer__emptyText">{emptyText}</p>
+          </>
+        )}
+      </div>
     </div>
   );
+};
+
+CoaChangelogContainer.defaultProps = {
+  title: 'Changelog',
+  emptyText: 'No entries in the changelog yet'
+};
+
+CoaChangelogContainer.propTypes = {
+  title: PropTypes.string,
+  emptyText: PropTypes.string
 };
