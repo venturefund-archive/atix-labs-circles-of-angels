315b8039f2110287b6b24812c7198b7ff79a296d
diego.aguirre02 <diego.aguirre02@globant.com>
2023-01-11 15:09:44 -0500

    feat(evidences details): :sparkles: add previewers for attached files of evidences - for image and pdf's

--- components/organisms/AttachedFiles/AttachedFiles.jsx
+++ components/organisms/AttachedFiles/AttachedFiles.jsx
@@ -1,31 +1,40 @@
-import { PaperClipOutlined } from '@ant-design/icons';
 import React from 'react';
 import { DictionaryContext } from 'components/utils/DictionaryContext';
+import { lookup } from 'mime-types';
+import { Icon } from 'antd';
+import { CoaButton } from 'components/atoms/CoaButton/CoaButton';
 
 export default function AttachedFiles({ files }) {
   const { texts } = React.useContext(DictionaryContext);
   return (
-    <div className='attached-files'>
-      <h2 className='attached-files__header'>{texts?.attachedFiles?.title || 'Attached Files'}</h2>
-      {
-        files && files.map((file) => (
-          <div className='attached-files__file' key={file.name}>
-            <div className='attached-files__file-left'>
-              <PaperClipOutlined />
-              <h4 className='attached-files__file-title'>{file.name}</h4>
-            </div>
-            <a
-              className='attached-files__file-download'
-              href={`${process.env.NEXT_PUBLIC_URL_HOST}${file.path}`}
-              target="_blank"
-              rel='noopener noreferrer'
-              download
-            >
-              {texts?.general?.btnDownload || 'Download'}
-            </a>
+    <div className="attached-files">
+      <h2 className="attached-files__header">{texts?.attachedFiles?.title || 'Attached Files'}</h2>
+
+      {files &&
+        files.map(file => (
+          <div className="attached-files__file" key={file.name}>
+            {lookup(file.name).includes('image') && (
+              <>
+                <img src={`${process.env.NEXT_PUBLIC_URL_HOST}${file.path}`} alt={file.name} />
+                <CoaButton
+                  href={`${process.env.NEXT_PUBLIC_URL_HOST}${file.path}`}
+                  className="attached-files__file__button"
+                  type="primary"
+                >
+                  <Icon type="download" /> Download
+                </CoaButton>
+              </>
+            )}
+            {lookup(file.name).includes('pdf') && (
+              <iframe
+                title={file.name}
+                src={`${process.env.NEXT_PUBLIC_URL_HOST}${file.path}`}
+                width="100%"
+                height="100%"
+              ></iframe>
+            )}
           </div>
-        ))
-      }
+        ))}
     </div>
-  )
+  );
 }
