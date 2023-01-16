77ba1d9fc4126f7d683f205a1326d934cce5230c
diego.aguirre02 <diego.aguirre02@globant.com>
2023-01-16 15:46:44 -0500

    refactor(backoffice layout): :lipstick: correct layout when there is not sidebar

--- components/Layouts/BackOfficeLayout/BackOfficeLayout.jsx
+++ components/Layouts/BackOfficeLayout/BackOfficeLayout.jsx
@@ -4,13 +4,20 @@ import { Layout } from 'antd';
 import Navbar from 'components/atoms/Navbar/Navbar';
 import SideBar from 'components/organisms/SideBar/SideBar';
 import './back-office-layout.scss';
+import classNames from 'classnames';
 
 const BackOfficeLayout = ({ children, user, project }) => (
   <Layout className="backOfficeLayout">
     <Navbar project={project} isProtectedRoute />
     <Layout className="mainContent">
       {user?.isAdmin && <SideBar hasDaos={user && user.hasDaos} role={user && user.role} />}
-      <div className="mainContent__content">{children}</div>
+      <div
+        className={classNames('mainContent__content', {
+          '--withSidebar': user?.isAdmin
+        })}
+      >
+        {children}
+      </div>
     </Layout>
   </Layout>
 );
