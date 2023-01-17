865c84783019f007b31a2d2cd4c8fb5832094074
diego.aguirre02 <diego.aguirre02@globant.com>
2023-01-17 08:56:02 -0500

    fix(evidence detail): :bug: should show the createdBy name and not just beneficiary always

--- components/molecules/EvidenceDetailBox/EvidenceDetailBox.jsx
+++ components/molecules/EvidenceDetailBox/EvidenceDetailBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CoaTag } from 'components/atoms/CoaTag/CoaTag';
 import { EVIDENCE_STATUS_MAP } from 'model/evidence';
-import './_style.scss'
+import './_style.scss';
 import { Divider } from 'antd';
 import { DictionaryContext } from 'components/utils/DictionaryContext';
 import { getDateAndTime } from '../../../helpers/utils';
@@ -10,22 +10,21 @@ import { getDateAndTime } from '../../../helpers/utils';
 export default function EvidenceDetailBox(props) {
   const { texts } = React.useContext(DictionaryContext);
 
-  const { title, description, createdAt, beneficiary, status, activity } = props;
+  const { title, description, createdAt, user, status, activity } = props;
   const date = new Date(createdAt);
 
-  const { firstName: fnBeneficiary , lastName: lnBeneficiary } = beneficiary || {};
-  const beneficiaryName = fnBeneficiary || lnBeneficiary? `${fnBeneficiary} ${lnBeneficiary}`: 'No name';
+  const { firstName: userFirstName, lastName: userLastName } = user || {};
+  const createdByName =
+    userFirstName || userLastName ? `${userFirstName} ${userLastName}` : 'No name';
 
-  const { firstName: fnAuditor , lastName: lnAuditor } = activity?.auditor || {};
-  const auditorName = fnAuditor || lnAuditor? `${fnAuditor} ${lnAuditor}`: 'No name';
+  const { firstName: fnAuditor, lastName: lnAuditor } = activity?.auditor || {};
+  const auditorName = fnAuditor || lnAuditor ? `${fnAuditor} ${lnAuditor}` : 'No name';
 
   return (
     <div className="evidence-container">
       {/* Evidence header */}
       <div className="evidence-container__header">
-        <h3 className="evidence-container__date">
-          {getDateAndTime(date)}
-        </h3>
+        <h3 className="evidence-container__date">{getDateAndTime(date)}</h3>
         {status && (
           <CoaTag predefinedColor={EVIDENCE_STATUS_MAP[status].color}>
             {EVIDENCE_STATUS_MAP[status].name}
@@ -34,24 +33,22 @@ export default function EvidenceDetailBox(props) {
       </div>
       <Divider style={{ marginTop: '0' }}></Divider>
       {/* Evidence indicator */}
-      <h3 className="evidence-container__indicator">{texts?.evidenceDetail?.title || 'EVIDENCE'}</h3>
+      <h3 className="evidence-container__indicator">
+        {texts?.evidenceDetail?.title || 'EVIDENCE'}
+      </h3>
       {/* Evidence title */}
       <h2 className="evidence-container__title">{title}</h2>
       <p className="evidence-container__description">{description}</p>
-      <div className='evidenceDetailBox__member'>
+      <div className="evidenceDetailBox__member">
         {/* Evidence creator */}
         <h3 className="evidenceDetailBox__member__Label">
           {texts?.evidenceDetail?.createdBy || 'Created by'}{' '}
-          <b className="evidenceDetailBox__member__LabelValue">
-            {beneficiaryName}
-          </b>
+          <b className="evidenceDetailBox__member__LabelValue">{createdByName}</b>
         </h3>
         {/* Auditor */}
         <h3 className="evidenceDetailBox__member__Label">
           {texts?.evidenceDetail?.auditor || 'Auditor'}:{' '}
-          <b className="evidenceDetailBox__member__LabelValue">
-            {auditorName}
-          </b>
+          <b className="evidenceDetailBox__member__LabelValue">{auditorName}</b>
         </h3>
       </div>
     </div>
@@ -62,9 +59,9 @@ EvidenceDetailBox.defaultProps = {
   title: '',
   description: '',
   createdAt: '',
-  beneficiary: {},
+  user: {},
   status: '',
-  auditor: {},
+  auditor: {}
 };
 
 EvidenceDetailBox.propTypes = {
@@ -72,12 +69,12 @@ EvidenceDetailBox.propTypes = {
   description: PropTypes.string,
   createdAt: PropTypes.string,
   status: PropTypes.string,
-  beneficiary: PropTypes.shape({
+  user: PropTypes.shape({
     firstName: PropTypes.string,
-    lastName: PropTypes.string,
+    lastName: PropTypes.string
   }),
   auditor: PropTypes.shape({
     firstName: PropTypes.string,
-    lastName: PropTypes.string,
+    lastName: PropTypes.string
   })
 };
